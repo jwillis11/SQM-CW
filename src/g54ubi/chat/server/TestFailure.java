1ffa248d9722ab46ee7a153d114958f564c23159
jw <jwillis11@live.co.uk>
2015-12-10 14:07:12 +0000

    fixing travis

---
+++ src/g54ubi/chat/server/TestFailure.java
@@ -0,0 +1,13 @@
+package g54ubi.chat.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFailure {
+
+	@Test
+	public void testSuccess() throws Exception {
+		//fail();
+	}
+}
---
+++ src/g54ubi/chat/server/test.java
@@ -0,0 +1,11 @@
+package g54ubi.chat.server;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestFailure.class })
+public class test {
+
+}
